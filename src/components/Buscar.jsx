5bbc6a36e851fad820245532587d9471e33af902
davidvalenciaz <mkcallese@gmail.com>
2024-01-25 07:00:30 -0500

    mañana

--- src/components/Buscar.jsx
+++ src/components/Buscar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 const users =[
   "jaime",
@@ -6,12 +7,29 @@ const users =[
   "isaac"
 ]
 
-function Buscar() {
+
+ export function Buscar() {
+
+  const buscar = () => {
+    [search,setSearch ] = useState("")
+    const handleBuscar = {name} => {
+      if(name .target.value === ""){
+        console.log("no hay valor ")
+      }
+    }
+
+
   return (
     <div className="container">
       <h1>opcion buscar</h1>
-      <input type="text" name="search" placeholder="buscar usuarios" onChange={} />
+      <input type="text" name="search" placeholder="buscar usuarios" onChange={handleBuscar} />
+      <ul>
+        {users.map((user,i)=>
+        <li key={i}>{user} </li>
+        )
 
+
+      </ul>
     </div>
   )
 }
