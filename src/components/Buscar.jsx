e871bff58da7f61259e47e10a946b1cbca1c95fb
davidvalenciaz <mkcallese@gmail.com>
2024-01-25 06:44:10 -0500

    cambios

--- src/components/Buscar.jsx
+++ src/components/Buscar.jsx
@@ -1,8 +1,18 @@
 
+const users =[
+  "jaime",
+  "diber",
+  "rios",
+  "isaac"
+]
 
 function Buscar() {
   return (
-    <div className="container">Buscar</div>
+    <div className="container">
+      <h1>opcion buscar</h1>
+      <input type="text" name="search" placeholder="buscar usuarios" onChange={} />
+
+    </div>
   )
 }
 
